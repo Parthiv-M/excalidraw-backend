fd454052d34e5378cb32e5377f4ab69d07537219
Parthiv Menon <parthivmenon01@gmail.com>
2021-07-17 15:18:32 +0530

    added getfile and download routes

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-const path = require("path");
 const express = require("express");
 const app = express();
 // const cors = require("cors");
@@ -10,19 +8,11 @@ const fs = require("fs");
 const port = process.env.PORT || 3001;
 
 // use express middleware
-// app.use(cors());
 app.use(express.json({ extended: false }));
-
-// buffer route
-app.get('*', (req, res) => {
-    
-});
 
 const saveFile = async (file, fileName) => {
   const data = fs.readFileSync(file.path);
-  console.log(path.join(__dirname));
-  // fs.writeFileSync(`${path.join(__dirname, "../../", "public/uploads")}/${fileName}.png`, data); 
-  fs.writeFileSync(`${__dirname}/${fileName}.png`, data);
+  fs.writeFileSync(`${__dirname}/uploads/${fileName}.png`, data);
   await fs.unlinkSync(file.path);
   return;
 };
@@ -30,11 +20,38 @@ const saveFile = async (file, fileName) => {
 app.post("/api/upload", (req, res) => {
     const form = new formidable.IncomingForm();
     form.parse(req, async function (err, fields, files) {
-      // console.log(fields.fileName.substring(fields.fileName.lastIndexOf("/") + 1, fields.fileName.length - 1))
       await saveFile(files.file, fields.fileName.substring(fields.fileName.lastIndexOf("/") + 1, fields.fileName.length - 1));
       return res.status(200).send({ success: true, message: "File uploaded successfully" });
     });
+});
+
+app.get("/api/getFiles", (req, res) => {
+    try {
+        fs.readdir(`${__dirname}/uploads`, (err, files) => {
+            if(files.length === 0) {
+                return res.status(401).send({ success: false, message: "No files in uploads" })
+            }
+            res.status(200).send({ success: true, files: files })
+        })
+    } catch(err) {
+        console.log(err);
+        return res.status(501).send({ success: false, message: "Oops, server error" })
+    }
 })
+
+app.get("/api/download", (req, res) => {
+    if(!req.query || !req.query.fileName) {
+        return res.status(401).send({ success: false, message: "No file name" })
+    }
+    const reqFile = req.query.fileName;
+    const filePath = `${__dirname}/uploads/${reqFile}`;
+    res.download(filePath)
+})
+
+// buffer route
+app.get('*', (req, res) => {
+    res.status(200).send({ message: "Oops, nothing to see here" })  
+});
 
 // listens to the app on PORT
 app.listen(port, () => {
